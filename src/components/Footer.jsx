2799980433549a3f1b83e31d226af11c847711db
RN88106 <official.rn88106@gmail.com>
2023-01-05 12:57:47 +0530

    fresh commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TECH-RAJA.</h1>
+        <p>@all right reserved - 2023 </p>
+      </div>
+
+    </footer>
+  );
+};
+
+export default Footer;
